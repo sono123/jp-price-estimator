804c77eb7c116e48db6228ff7125fd828011d9cc
sono123 <steveono@gmail.com>
2015-12-21 00:57:12 -0500

    Add CreateInk model and migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221054940) do
+ActiveRecord::Schema.define(version: 20151221055505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ink_colors", force: :cascade do |t|
+    t.integer  "front"
+    t.integer  "back"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "print_methods", force: :cascade do |t|
     t.string   "print_method"
