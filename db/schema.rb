bf21ec16a6bb0801eb0146e1e94983493456a913
sono123 <steveono@gmail.com>
2015-12-21 00:58:54 -0500

    Add Bleed model and migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221055505) do
+ActiveRecord::Schema.define(version: 20151221055823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bleeds", force: :cascade do |t|
+    t.boolean  "front"
+    t.boolean  "back"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "ink_colors", force: :cascade do |t|
     t.integer  "front"
