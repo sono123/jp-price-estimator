5dacf53ec29d5fdae69c5c696ddfc1fba344e0f0
sono123 <steveono@gmail.com>
2015-12-21 01:35:10 -0500

    Add Dimension model and migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221063056) do
+ActiveRecord::Schema.define(version: 20151221063406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20151221063056) do
   create_table "bleeds", force: :cascade do |t|
     t.boolean  "front"
     t.boolean  "back"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dimensions", force: :cascade do |t|
+    t.float    "width"
+    t.float    "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
