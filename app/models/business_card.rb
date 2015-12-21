466d671e2ccafbcd5c84146b6625a896ab722caa
sono123 <steveono@gmail.com>
2015-12-21 17:49:00 -0500

    Add associations to model.

--- app/models/business_card.rb
+++ app/models/business_card.rb
@@ -1,3 +1,10 @@
 class BusinessCard < ActiveRecord::Base
   belongs_to :print_method
+  belongs_to :ink_color
+  belongs_to :bleed
+  belongs_to :raised_ink
+  belongs_to :dimension
+  belongs_to :paper_type
+  belongs_to :coating
+  belongs_to :quantity
 end
