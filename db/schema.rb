5379a000d9b4a9190c147cac8fa9066fabdc84cb
sono123 <steveono@gmail.com>
2015-12-21 01:39:19 -0500

    Add Coating model and migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221063657) do
+ActiveRecord::Schema.define(version: 20151221063832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20151221063657) do
   create_table "bleeds", force: :cascade do |t|
     t.boolean  "front"
     t.boolean  "back"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "coatings", force: :cascade do |t|
+    t.string   "front"
+    t.string   "back"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
