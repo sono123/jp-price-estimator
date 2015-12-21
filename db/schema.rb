e6f25a23f4d2bacdbb27320aaf954aa7a60a4b0e
sono123 <steveono@gmail.com>
2015-12-21 01:40:56 -0500

    Add Quantity model and migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221063832) do
+ActiveRecord::Schema.define(version: 20151221064022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,12 @@ ActiveRecord::Schema.define(version: 20151221063832) do
     t.string   "print_method"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "quantities", force: :cascade do |t|
+    t.integer  "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "raised_inks", force: :cascade do |t|
