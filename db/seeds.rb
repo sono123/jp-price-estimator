2b1f7e120d13a00f3712a9ea0b46bf7b600079a9
sono123 <steveono@gmail.com>
2015-12-21 22:56:21 -0500

    Add seeds for various specs, but not business card objects.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,84 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Print Methods
+print_methods = ["pantone offset", "cmyk offset", "letterpress", "digital"]
+print_methods.each do |name|
+	PrintMethod.create(print_method: name)
+end
+
+
+# Ink Colors
+(0..4).each do |front|
+	(0..4).each do |back|
+		InkColor.create(front: front, back: back)
+	end
+end
+
+
+# Bleeds
+[true, false].each do |front|
+	[true, false].each do |back|
+		Bleed.create(front: front, back: back)
+	end
+end
+
+
+# Raised Inks
+(0..4).each do |front|
+	(0..4).each do |back|
+		RaisedInk.create(front: front, back: back)
+	end
+end
+
+
+# Dimensions
+Dimension.create(width: 3.5, height: 2)
+Dimension.create(width: 2.5, height: 2.5)
+Dimension.create(width: 6, height: 4)
+
+
+# Paper Types
+PaperType.create(brand: "neenah", name: "classic crest", color: "solar white", thickness: 130)
+PaperType.create(brand: "neenah", name: "classic crest", color: "natural white", thickness: 130)
+PaperType.create(brand: "neenah", name: "classic linen", color: "solar white", thickness: 130)
+PaperType.create(brand: "mohawk", name: "strathmore", color: "ultimate white", thickness: 130)
+PaperType.create(brand: "mohawk", name: "strathmore", color: "bright white", thickness: 130)
+PaperType.create(brand: "mohawk", name: "strathmore", color: "natural white", thickness: 130)
+
+
+# Coatings
+coating_types = [["uv coating", "none"], 
+								 ["dull matte", "none"], 
+								 ["silk laminate"], 
+								 ["suede laminate"], 
+								 ["varnish", "none"]] 
+coating_types.each do |arr|
+	arr.each do |front|
+		arr.each do |back|
+			Coating.create(front: front, back: back)
+		end
+	end
+end
+
+
+# Quantities
+quantity_increments = [250, 500, 1000, 1500, 2000, 2500]
+quantity_increments.each do |qty|
+	Quantity.create(quantity: qty)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
