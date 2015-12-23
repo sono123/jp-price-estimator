7f37415443e51408023644d908fddeb26792a595
sono123 <steveono@gmail.com>
2015-12-23 00:24:42 -0500

    Add search class method to business card model.

--- app/models/business_card.rb
+++ app/models/business_card.rb
@@ -7,4 +7,16 @@ class BusinessCard < ActiveRecord::Base
   belongs_to :paper_type
   belongs_to :coating
   belongs_to :quantity
+
+  def self.search(q)
+  	where(print_method_id: q.print_method, 
+  				ink_color_id: q.ink_color, 
+  				bleed_id: q.bleed, 
+  				raised_ink_id: q.raised_ink,
+  				dimension_id: q.dimension,
+  				paper_type_id: q.paper_type,
+  				coating_id: q.coating,
+  				quantity_id: q.quantity
+  				)
+  end
 end
