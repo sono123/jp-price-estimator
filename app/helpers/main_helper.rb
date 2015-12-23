811a9bee0b725a6638c58adc551241bd7a44f841
sono123 <steveono@gmail.com>
2015-12-22 23:49:20 -0500

    Add the rest of the helpers and select tags.

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -13,7 +13,7 @@ module MainHelper
 		@bleeds.map do |i|
 			front = ""
 			back = ""
-			
+
 			if i.front == true
 				front << "1"
 			else
@@ -28,6 +28,57 @@ module MainHelper
 
 			[front + "/" + back, i.id]
 		end
+	end
 
+	def raised_inks
+		@raised_inks.map do |i|
+			[i.front.to_s + "/" + i.back.to_s, i.id]
+		end
+	end
+
+	def trim(num)
+		int = num.to_i
+		flot = num
+		int == flot ? int : flot
+	end
+
+	def dimensions
+		@dimensions.map do |i|
+			[trim(i.width).to_s + " x " + trim(i.height).to_s, i.id]
+		end
+	end
+
+	def paper_types
+		@paper_types.map do |i|
+			[i.brand + " - " + i.name + " - " + i.color + " - " + i.thickness.to_s + "lb", i.id]
+		end
+	end
+
+	def coatings
+		@coatings.map do |i|
+			[i.front + " / " + i.back, i.id]
+		end
+	end
+
+	def quantities
+		@quantities.map do |i|
+			[i.quantity, i.id]
+		end
 	end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
