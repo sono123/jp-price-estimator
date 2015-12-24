523d7e969925e6c5c12bb31c4833dddd7cb3d15b
sono123 <steveono@gmail.com>
2015-12-24 01:09:37 -0500

    Add strong params.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
   def index
+  	@business_card = BusinessCard.new
   	@print_methods = PrintMethod.all
   	@ink_colors = InkColor.all
   	@bleeds = Bleed.all
@@ -12,6 +13,55 @@ class MainController < ApplicationController
 
   def search
   	@result = BusinessCard.search(params)
-  	redirect_to root_path
+  	if @result
+  		redirect_to root_path # with ajax eventually
+  	else
+  		# use ajax to alter the form HTML without deleting the values
+  	end
   end
+
+  def create
+  	@business_card = BusinessCard.create(business_card_params)
+  	redirect_to root_path # with ajax eventually
+  end
+
+  private
+
+  def business_card_params
+  	params.require(:business_card).permit(:print_method_id, 
+  																				:ink_color_id, 
+  																				:bleed_id, 
+  																				:raised_ink_id, 
+  																				:dimension_id, 
+  																				:paper_type_id, 
+  																				:coating_id, 
+  																				:quantity_id,
+  																				:price)
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
