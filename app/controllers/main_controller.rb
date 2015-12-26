0cc1cd8df00c50c5f8f605db6aa2db26867967eb
sono123 <steveono@gmail.com>
2015-12-26 01:33:52 -0500

    Add business card controller and move business card create method into that controller.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -22,33 +22,6 @@ class MainController < ApplicationController
   	end
   end
 
-  def create
-  	search = BusinessCard.search(params["business_card"])
-  	@result = search[0]
-  	if @result
-  		flash[:error] = "That item is already in the system."
-  		redirect_to root_path
-  	else
-	  	BusinessCard.create(business_card_params)
-	  	flash[:success] = "Price successfully added."
-	  	redirect_to root_path
-  	end
-  end
-
-  private
-
-  def business_card_params
-  	params.require(:business_card).permit(:print_method_id, 
-  																				:ink_color_id, 
-  																				:bleed_id, 
-  																				:raised_ink_id, 
-  																				:dimension_id, 
-  																				:paper_type_id, 
-  																				:coating_id, 
-  																				:quantity_id,
-  																				:price)
-  end
-
 end
 
 
