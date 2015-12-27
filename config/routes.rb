f9ebe54088fe6bc5480a6457d56c21b6f9ceec95
sono123 <steveono@gmail.com>
2015-12-27 01:04:26 -0500

    Add dimensions controller scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/business_cards/:id/edit' => 'business_cards#edit'
   patch '/business_cards/:id' => 'business_cards#update'
 
+  resources :dimensions
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
