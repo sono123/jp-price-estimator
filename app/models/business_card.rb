3420d663fdf764c89a64da39d60efae3c5291b88
sono123 <steveono@gmail.com>
2015-12-28 23:34:27 -0500

    Style dimensions index.

--- app/models/business_card.rb
+++ app/models/business_card.rb
@@ -9,22 +9,6 @@ class BusinessCard < ActiveRecord::Base
   belongs_to :quantity
 
   def self.search(q)
-  	puts "REACHED BUSINESS-CARD SEARCH CLASS METHOD****************"
   	result = where("print_method_id = ? AND ink_color_id = ? AND bleed_id = ? AND raised_ink_id = ? AND dimension_id = ? AND paper_type_id = ? AND coating_id = ? AND quantity_id = ?", q['print_method_id'], q['ink_color_id'], q['bleed_id'], q['raised_ink_id'], q['dimension_id'], q['paper_type_id'], q['coating_id'], q['quantity_id'])
-  	p "*******************reached result****************"
-  	p result
   end
 end
-
-
-
-
-
-# print_method_id: q['print_method_id'], 
-# ink_color_id: q['ink_color_id'], 
-# bleed_id: q['bleed_id'], 
-# raised_ink_id: q['raised_ink_id'],
-# dimension_id: q['dimension_id'],
-# paper_type_id: q['paper_type_id'],
-# coating_id: q['coating_id'],
-# quantity_id: q['quantity_id']
